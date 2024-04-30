11d345f255c5185b6e8dd4fa0be72de82a6bb027
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-04-30 17:13:24 +0200

    Update SkodaCar.java

--- new/SkodaCar.java
+++ new/SkodaCar.java
@@ -1,15 +1,7 @@
 package generics;
 
 public class SkodaCar extends Car {
-   //наследени полета
-    //brand
-    //owner
-    //hp
-
-
-    //наследени методи
-    //drive
-    //refuel
+ 
 
     public void driveSkoda () {
         System.out.println("Drive Skoda");

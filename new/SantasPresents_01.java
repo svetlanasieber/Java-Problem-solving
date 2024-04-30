3b687e327bff2ce4943c3ede734cb19603e404d9
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-04-30 17:16:16 +0200

    Update SantasPresents_01.java

--- new/SantasPresents_01.java
+++ new/SantasPresents_01.java
@@ -18,7 +18,7 @@ public class SantasPresents_01 {
                                 .map(Integer::parseInt)
                                 .collect(Collectors.toCollection(ArrayDeque::new));
 
-        //играчка -> бр. произведени
+      
         Map<String, Integer> presentsMap = new TreeMap<>();
         presentsMap.put("Doll", 0);
         presentsMap.put("Wooden train", 0);
@@ -26,30 +26,30 @@ public class SantasPresents_01 {
         presentsMap.put("Bicycle", 0);
 
         while (!materials.isEmpty() && !magics.isEmpty()) {
-            //първия материал
+      
             int material = materials.peek();
             int magic = magics.peek();
 
             int totalMagic = magic * material; //получената магия
 
-            //проверка за получената магия
+          
             if (totalMagic == 150) {
-                //кукла
+              
                 materials.pop();
                 magics.poll();
                 presentsMap.put("Doll", presentsMap.get("Doll") + 1);
             } else if (totalMagic == 250) {
-                //влакче
+             
                 materials.pop();
                 magics.poll();
                 presentsMap.put("Wooden train", presentsMap.get("Wooden train") + 1);
             } else if (totalMagic == 300) {
-                //мече
+             
                 materials.pop();
                 magics.poll();
                 presentsMap.put("Teddy bear", presentsMap.get("Teddy bear") + 1);
             } else if (totalMagic == 400) {
-                //колело
+             
                 materials.pop();
                 magics.poll();
                 presentsMap.put("Bicycle", presentsMap.get("Bicycle") + 1);
@@ -71,20 +71,18 @@ public class SantasPresents_01 {
             }
         }
 
-        //проверка дали сме произвели нужните подаръци
-        //1. кукла и влакче
-        //2. мече и колело
+    
         boolean isDollAndTrain = presentsMap.get("Doll") > 0 && presentsMap.get("Wooden train") > 0;
         boolean isBearAndBicycle = presentsMap.get("Teddy bear") > 0 && presentsMap.get("Bicycle") > 0;
         if (isDollAndTrain || isBearAndBicycle) {
-            //справили сме се с изработката
+          
             System.out.println("The presents are crafted! Merry Christmas!");
         } else {
-            //не сме се справили с изработката
+          
             System.out.println("No presents this Christmas!");
         }
 
-        //останалите материали
+     
         if (!materials.isEmpty()) {
             System.out.print("Materials left: ");
             //[3, 4, 5]
@@ -92,17 +90,14 @@ public class SantasPresents_01 {
             System.out.println(materials.toString().replace("[", "").replace("]", ""));
         }
 
-        //останалите магии
+      
         if (!magics.isEmpty()) {
             System.out.print("Magic left: ");
             System.out.println(magics.toString().replace("[", "").replace("]", ""));
         }
 
-        //отпечатваме: играчка: брой > 0
         for (Map.Entry<String, Integer> entry : presentsMap.entrySet()) {
-            //entry
-            //key: играчка
-            //value: бройка
+
             if (entry.getValue() > 0) {
                 System.out.println(entry.getKey() + ": " + entry.getValue());
             }

2ffd5d6aa062c5200c1aadb669181fb0f4ae2102
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2024-05-02 16:37:02 +0200

    Update SoftuniBarIncome_02.java

--- RegEx/SoftuniBarIncome_02.java
+++ RegEx/SoftuniBarIncome_02.java
@@ -8,25 +8,25 @@ public class SoftuniBarIncome_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String regex = "%(?<customerName>[A-Z][a-z]+)%[^\\|$%\\.]*<(?<product>\\w+)>[^\\|\\$%\\.]*\\|(?<count>[0-9]+)\\|[^\\|\\$%\\.]*?(?<price>[0-9]+\\.?[0-9]*)\\$";
-        Pattern pattern = Pattern.compile(regex); //шаблон
+        Pattern pattern = Pattern.compile(regex); 
 
-        double totalIncome = 0; //приход от всички поръчки
+        double totalIncome = 0; 
 
-        String input = scanner.nextLine(); //входни данни
+        String input = scanner.nextLine(); 
         while (!input.equals("end of shift")) {
-            //входни данни представляват поръчка
-            //input = "%George%<Croissant>|2|10.3$"
+           
+           
             Matcher matcher = pattern.matcher(input);
 
             if (matcher.find()) {
-                //валидна поръчка
-                //matcher = "%(?<customerName>George)%<(?<product>Croissant)>|(?<count>2)|(?<price>10.3)$"
+               
+               
                 String customerName = matcher.group("customerName"); //"George"
                 String product = matcher.group("product"); //"Croissant"
                 int count = Integer.parseInt(matcher.group("count")); //"2" -> parse -> 2
                 double price = Double.parseDouble(matcher.group("price")); //"10.3" -> parse -> 10.3
 
-                double totalPricePerOrder = count * price; //цена за текушата поръчка
+                double totalPricePerOrder = count * price; 
                 totalIncome += totalPricePerOrder;
                 System.out.printf("%s: %s - %.2f%n", customerName, product, totalPricePerOrder);
             }

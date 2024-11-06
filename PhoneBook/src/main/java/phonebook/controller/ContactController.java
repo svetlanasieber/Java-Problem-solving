8b02be5efb955cb3f3fc382973ee392509c95895
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-06 02:17:59 +0100

    Update ContactController.java

--- PhoneBook/src/main/java/phonebook/controller/ContactController.java
+++ PhoneBook/src/main/java/phonebook/controller/ContactController.java
@@ -8,25 +8,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class ContactController {   //This class will list and save contacts
+public class ContactController {  
     private List<Contact> contacts;
 
     public ContactController() {
         this.contacts = new ArrayList<>();
     }
 
-    @GetMapping("/")   //tells Spring that this method cannot be called if the user wants to submit data. It should be only used for viewing data.
-    // ModelAndView allows us to pass all the information required by Spring MVC in one return
-    public ModelAndView index(ModelAndView modelAndView) {  //add contacts to the modelAndView and return it as a result
+    @GetMapping("/") 
+  
+    public ModelAndView index(ModelAndView modelAndView) {  
         modelAndView.setViewName("index");
         modelAndView.addObject("contacts", contacts);
         return modelAndView;
     }
 
-    @PostMapping("/")           //told Spring that this method expects data that it needs to autofill in contact object
-    public String add (Contact contact) {  //add the contact in our list and give a response to the user
+    @PostMapping("/")          
+    public String add (Contact contact) { 
         //validation
         this.contacts.add(contact);
-        return "redirect:/";    //redirect the user back to index page as response
-    }
+        return "redirect:/";  
 }
